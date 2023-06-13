4b03c95c44dadeaaa663d4c149b9499ad46f94fe
Mounika Manchala <mounikamanchala25@gmail.com>
2023-06-13 10:20:22 +0530

    removed security & added port number method

--- src/main/java/com/example/demo/OrderServiceApplication.java
+++ src/main/java/com/example/demo/OrderServiceApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class OrderServiceApplication {
 
+	private Environment environment;
+
+	public OrderServiceApplication(Environment environment) {
+		super();
+		this.environment = environment;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
@@ -23,4 +31,9 @@ public class OrderServiceApplication {
 		return ResponseEntity.ok("hello" + name);
 	}
 
+	@GetMapping("/status")
+	public ResponseEntity<?> getPort() {
+		return ResponseEntity
+				.ok("Order Service up & running with port " + environment.getProperty("local.server.port"));
+	}
 }
